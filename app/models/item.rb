8d300ef2e0204d6a2da3fd565b46b8a3813dce29
Luca Montaigut <lmontaigut@gmail.com>
2020-03-09 10:47:30 +0100

    validates ok

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  validates :title, 
+    presence: true,
+    length: { in: 3..50 }
+  validates :description,
+    length: { maximum: 1024 }
+  validates :price,
+    numericality: { greater_than: 0 }
+  validates :image_url,
+    presence: true
 end
