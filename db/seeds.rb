75ea27693b23f8b30f2d4f9e01fff50dc5a7d5b3
mariuslhl <mariuslhl@gmail.com>
2020-03-09 18:03:42 +0100

    fixing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.destroy_all
+
+20.times do
+    item = Item.new(title: "Chaton Abyssin de 3 mois", description: "Chaton Abyssin trop mignon! Les grands yeux et les oreilles remarquables du chat Abyssin en disent long sur le caractère de cette race de chat.  Les Abyssins sont des chats très proches de l'homme et font preuve d’une curiosité exacerbée.", price: 500)
+    item.picture.attach(io: File.open('/home/marius/Documents/Projet_chaton_bis/Projet_Chatons/db/mask_cat.jpg'), filename: 'cat.jpg', content_type: 'image/jpeg')
+    item.save
+end
