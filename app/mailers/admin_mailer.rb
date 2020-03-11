703ee14a170dbac612895edd75288401c1916b56
thperchi <thibault.perchicot@outlook.fr>
2020-03-11 18:08:49 +0100

    mails admin done

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,10 @@ class AdminMailer < ApplicationMailer
 
 	def order_mail(order)
 		@order = order
+		admins = User.select { |user| user.is_admin == true }
+		admins.each do |admin|
+			mail(to: admin.email, subject: "Nouvelle commande")
+		end
 	end
 
 end
