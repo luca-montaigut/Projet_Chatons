23ac29064318687e4b4ce57b04c29953876126dc
Luca Montaigut <lmontaigut@gmail.com>
2020-03-11 11:26:43 +0100

    guest_cart ok and shove items from guest_cart to user_cart ok

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,4 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
   
   def show
   end
@@ -11,16 +9,10 @@ class CartsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    join = item.join_id(current_user)
-    puts join
-    JoinCartItem.destroy(join.ids)
+    JoinCartItem.find_by(cart_id: @cart.id, item_id: item.id).destroy
   end
 
   private
-
-  def set_cart
-    @cart = current_user.cart
-  end
 
   def cart_params
     params.require(:cart).permit(:user_id, :id)
