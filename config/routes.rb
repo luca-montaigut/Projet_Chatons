d2e95d7d2d8b48040e7534cdc5dea370b6135b21
Luca Montaigut <lmontaigut@gmail.com>
2020-03-12 20:07:52 +0100

    better routes names

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
   devise_for :users
-  resources :items do 
+  resources :items, path: 'photo_de_chat' do 
     resources :rating, only: [:update]
-    resources :comments, only: [:create, :edit, :update, :destroy]
+    resources :comments, only: [:create, :edit, :update, :destroy], path: 'mon_commentaire'
   end
-  resources :carts, only: [:show, :update, :destroy]
-  resources :users, only: [:show, :update, :edit]
-  resources :charges, only: [:new, :create]
+  resources :carts, only: [:show, :update, :destroy], path: 'mon_panier'
+  resources :users, only: [:show, :update, :edit], path: 'mon_profil'
+  resources :charges, only: [:new, :create], path: 'paiement'
 
   scope 'admin', module: 'admin', as: 'admin' do
     resources :dashboard, only: [:index]
