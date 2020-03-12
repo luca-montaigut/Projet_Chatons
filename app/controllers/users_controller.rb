46ca8f35fff8bcc05202847237c7667c6c12ceb5
mariuslhl <mariuslhl@gmail.com>
2020-03-12 17:44:42 +0100

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = current_user
     user_params =  params.require(:user).permit(:first_name, :last_name, :birthdate, :description, :adress)
     @user.update(user_params)
     redirect_to user_path(@user.id)
