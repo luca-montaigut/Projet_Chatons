c55456268901399326b9df0a52af4ada1a7b543f
Luca Montaigut <lmontaigut@gmail.com>
2020-03-12 09:59:31 +0100

    controller marius

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,13 +5,7 @@ class CartsController < ApplicationController
 
   def update
     
-    in_cart = JoinCartItem.find_by(cart_id: @cart.id, item_id: Item.find(params[:id]))
-
-    if in_cart != nil
-      in_cart.update(quantity: in_cart.quantity.to_i + params[:quantity].to_i)  
-    else
-      JoinCartItem.create(cart_id: @cart.id, item_id: Item.find(params[:id]).id, quantity: params[:quantity])
-    end
+    JoinCartItem.create(cart_id: @cart.id, item_id: Item.find(params[:id]).id, quantity: params[:quantity])
 
     respond_to do |format|
       format.js  
